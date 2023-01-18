ce7f801adfed8f0cd3339b6193a88a9a65d60210
marcoperinidev <info@mpdev.me>
2023-01-19 00:18:47 +0100

    aggiunto toFixed for temperature

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp}°C</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
           {data.weather ? <p>{data.weather[0].main}</p> : null}
